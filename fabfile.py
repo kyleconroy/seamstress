ee45f3ed73e778d933b7be4340614516b9371395
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-01-15 13:52:10 -0800

    Added the user and document resource

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,15 @@
+import random
 from fabric.api import *
 from fabric.colors import magenta
 from seamstress import *
 from mock import patch
+from nose.tools import assert_equals, assert_in, assert_not_in, with_setup
+
+def add_user():
+    user("foo")
+
+def del_user():
+    user("foo", state="deleted")
 
 def assert_abort(func):
     @patch("seamstress.abort")
@@ -16,6 +24,57 @@ def assert_abort(func):
     actual_test.__name__ = func.__name__
     return actual_test
 
+# Users
+@assert_abort
+def test_user_invalid_state():
+    user("alice", state="foobar")
+
+def test_user_deletion():
+    user("alice", state="deleted")
+    users = sudo("cat /etc/passwd | awk -F: '{ print $1 }'")
+    assert_not_in("alice", [u for u in users.split("\r\n")])
+
+def test_user_creation():
+    user("alice")
+    users = sudo("cat /etc/passwd | awk -F: '{ print $1 }'")
+    assert_in("alice", [u for u in users.split("\r\n")])
+
+# Files
+@assert_abort
+def test_document_invalid_state():
+    document("foobar", state="foobar")
+
+@assert_abort
+def test_document_directory():
+    document("foobar", source="tests")
+
+@assert_abort
+def test_document_invalid_path():
+    document("foobar", source="foo/bar/man.txt")
+
+def test_document_create_file():
+    document("foobar", source=open("tests/hello.txt"))
+    contents = sudo("cat foobar")
+    assert_equals(contents, "Hello World")
+
+def test_document_create_contents():
+    document("foobar", source="tests/hello.txt")
+    contents = sudo("cat foobar")
+    assert_equals(contents, "Hello World")
+
+def test_document_create_mode():
+    document("foobar", mode=644)
+    assert_equals(sudo('stat -c "%a" foobar'), "644")
+
+def test_document_create():
+    document("foobar")
+    sudo("test -f foobar")
+
+def test_document_delete():
+    document("foobar", state="deleted")
+    sudo("test ! -f foobar")
+
+# Directory
 @assert_abort
 def test_directory_no_root():
     directory("/") 
@@ -24,19 +83,46 @@ def test_directory_no_root():
 def test_directory_invalid_state():
     directory("/var/web/upickem", state="foobar")
 
+def test_directory_create_mode():
+    directory("/var/web/boom", mode=666)
+    assert_equals(sudo('stat -c "%a" /var/web/boom'), "666")
+
 def test_directory_create():
     directory("/var/web/upickem")
     sudo("test -d /var/web/upickem")
 
 def test_directory_delete():
-    directory("/var/web/foobar",
-        state="deleted")
+    directory("/var/web/foobar", state="deleted")
     sudo("test ! -d /var/web/foobar")
+
+def test_directory_owner_group():
+    user("foo")
+    directory("/var/web/foobar", group="foo", owner="foo")
+    assert_equals(sudo('stat -c "%G" /var/web/foobar'), "foo")
+    assert_equals(sudo('stat -c "%U" /var/web/foobar'), "foo")
+    user("foo", state="deleted")
+
+def test_directory_owner():
+    user("foo")
+    directory("/var/web/foobar", owner="foo")
+    assert_equals(sudo('stat -c "%U" /var/web/foobar'), "foo")
+    user("foo", state="deleted")
+
+def test_directory_user():
+    user("foo")
+    directory("/var/web/foobar", group="foo")
+    assert_equals(sudo('stat -c "%G" /var/web/foobar'), "foo")
+    user("foo", state="deleted")
 
 @task
 def seamstress_test():
-    for (name, thing) in globals().iteritems():
-        if name.startswith("test"):
-            puts(magenta(name))
-            thing()
+    # setup
+    # sudo("adduser foo")
+
+    tests = [f for f in globals().iteritems() if f[0].startswith("test")]
+    random.shuffle(tests)
+
+    for (name, thing) in tests: 
+        puts(magenta(name))
+        thing()
 
--- seamstress.py
+++ seamstress.py
@@ -1,21 +1,69 @@
 __version_info__ = ('0', '0', '1')
 __version__ = '.'.join(__version_info__)
 
-from fabric.api import sudo
+import os
+from fabric.api import sudo, settings, hide, put
 from fabric.utils import abort
 
-def directory(path, state="created", owner=None, group=None):
+def document(path, source=None, state="created", mode=None):
+    if state not in ["created", "deleted"]:
+        abort(("A document can't be in state '%s'. "
+               "Valid states are 'created' or 'deleted'") % state)
+
+    if state == "deleted":
+        sudo("rm -f %s" % path)
+        return
+
+    if source is None:
+        sudo("touch %s" % path)
+        return
+
+    if isinstance(source, basestring) and not os.path.exists(source):
+        abort("No document exists at the local path %s" % source)
+
+    if isinstance(source, basestring) and not os.path.isfile(source):
+        abort("Specified path %s is a directory" % source)
+
+    put(source, path, use_sudo=True, mode=mode)
+
+
+def user(name, state="created", group=None, system=False):
+    if state not in ["created", "deleted"]:
+        abort(("A user can't be in state '%s'. "
+               "Valid states are 'created' or 'deleted'") % state)
+
+    if state == "deleted":
+        with settings(warn_only=True):
+            result = sudo("userdel %s" % name)
+            if result.failed and not result.return_code == 6:
+                abort("Error when deleting user %s. `userdel` command "
+                      "exited with status %s" % (name, result.return_code))
+    else:
+        with settings(warn_only=True):
+            result = sudo("useradd %s" % name)
+            if result.failed and not result.return_code == 9:
+                abort("Error when creating user %s. `useradd` command "
+                      "exited with status %s" % (name, result.return_code))
+
+
+def directory(path, state="created", owner=None, group=None, mode=None):
     if path == "/":
         abort("Configuring / is considered harmful")
 
-    if state == "created":
-        sudo("mkdir -p %s" % path)
-        return 
+    if state not in ["created", "deleted"]:
+        abort(("A directory can't be in state '%s'. "
+               "Valid states are 'created' or 'deleted'") % state)
 
     if state == "deleted":
         sudo("rm -rf %s" % path)
         return
 
-    abort(("A directory can't be in state '%s'. "
-           "Valid states are 'created' or 'deleted'") % state)
+    sudo("mkdir -p %s" % path)
+
+    if owner or group:
+        sudo("chown %s:%s %s" % (owner or "", group or "", path))
+
+    if mode:
+        sudo("chmod %s %s" % (mode, path))
+
 
