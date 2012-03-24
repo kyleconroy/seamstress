ec99bfb4c9e2b946b3578e27f393cbbe6e66f7c8
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-03-24 13:38:52 -0700

    Correct assert_abort test

--- tests/tail.py
+++ tests/tail.py
@@ -3,11 +3,11 @@ from fabric.api import *
 def assert_abort(func):
 
     def actual_test():
-        with settings(warn_only=True):
-            result = func()
-        if not result.failed:
+        try:
+            func()
             abort("%s should abort" % func.__name__) 
-        return result
+        except SystemExit:
+            return
 
     actual_test.__name__ = func.__name__
     return actual_test
